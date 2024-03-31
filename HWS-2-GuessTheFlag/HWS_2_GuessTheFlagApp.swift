616fc62f1659a1336b1762cc714df1acf854fa5f
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-03-31 23:23:36 +0530

    Initial Commit

---
+++ HWS-2-GuessTheFlag/HWS_2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_2_GuessTheFlagApp.swift
+//  HWS-2-GuessTheFlag
+//
+//  Created by Vaibhav Ranga on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HWS_2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
